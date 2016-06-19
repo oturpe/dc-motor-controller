76aee3ae6cbd753fe98cee3e5f2e4cd2ddb20282
Otto Urpelainen <oturpe@iki.fi>
2016-06-19 20:02:17 +0200

    Completed sample code

--- src/config.h
+++ src/config.h
@@ -5,17 +5,30 @@
 #define F_CPU 1000000UL
 
 // Delay between two executions of main loop, given in millisecond.
+
 #define LOOP_DELAY 100
+
+// Sets 1 MHz / 256 / 64 = 61 Hz pwm
+#define PRESCALER_VALUE Atmega8::PSV_64
+
+// Half length of one complete indicator period. In other words, the time the
+// indicator spends on and off. Given in units of LOOP_DELAY.
+#define INDICATOR_HALF_PERIOD 20
+
+// Speeds and accelerations of motors, in range 0 .. 255 where 255 is the
+// maximum.
+#define MOTOR_1_SPEED 0x80
+#define MOTOR_2_SPEED 0x80
+#define MOTOR_3_SPEED 0x80
+#define MOTOR_1_ACCELERATION 0x08
+#define MOTOR_2_ACCELERATION 0x08
+#define MOTOR_3_ACCELERATION 0x08
 
 // Definition of indicator pin data direction and data ports and pins
 #define INDICATOR_DATA_DIR DDRB
 #define INDICATOR_DATA_DIR_PIN DDB0
 #define INDICATOR_DATA PORTB
 #define INDICATOR_DATA_PIN PORTB0
-
-// Half length of one complete indicator period. In other words, the time the
-// indicator spends on and off. Given in units of LOOP_DELAY.
-#define INDICATOR_HALF_PERIOD 5
 
 // Definitions of switch data in input ports and pins
 #define SWITCH_1_DATA PORTD
@@ -53,29 +66,35 @@
 #define MOTOR_1_FORWARD_DATA_DIR_PIN DDB2
 #define MOTOR_1_FORWARD_DATA PORTB
 #define MOTOR_1_FORWARD_DATA_PIN PORTB2
+#define MOTOR_1_FORWARD_OUTPUT_COMPARE OCR1BL
 
 #define MOTOR_1_REVERSE_DATA_DIR DDRB
 #define MOTOR_1_REVERSE_DATA_DIR_PIN DDB1
 #define MOTOR_1_REVERSE_DATA PORTB
 #define MOTOR_1_REVERSE_DATA_PIN PORTB1
+#define MOTOR_1_REVERSE_OUTPUT_COMPARE OCR1AL
 
 #define MOTOR_2_FORWARD_DATA_DIR DDRB
 #define MOTOR_2_FORWARD_DATA_DIR_PIN DDB3
 #define MOTOR_2_FORWARD_DATA PORTB
 #define MOTOR_2_FORWARD_DATA_PIN PORTB3
+#define MOTOR_2_FORWARD_OUTPUT_COMPARE OCR2A
 
 #define MOTOR_2_REVERSE_DATA_DIR DDRD
 #define MOTOR_2_REVERSE_DATA_DIR_PIN DDD3
 #define MOTOR_2_REVERSE_DATA PORTD
 #define MOTOR_2_REVERSE_DATA_PIN PORTD3
+#define MOTOR_2_REVERSE_OUTPUT_COMPARE OCR2B
 
 #define MOTOR_3_FORWARD_DATA_DIR DDRD
 #define MOTOR_3_FORWARD_DATA_DIR_PIN DDD6
 #define MOTOR_3_FORWARD_DATA PORTD
 #define MOTOR_3_FORWARD_DATA_PIN PORTD6
+#define MOTOR_3_FORWARD_OUTPUT_COMPARE OCR0A
 
 #define MOTOR_3_REVERSE_DATA_DIR DDRD
 #define MOTOR_3_REVERSE_DATA_DIR_PIN DDD5
 #define MOTOR_3_REVERSE_DATA PORTD
 #define MOTOR_3_REVERSE_DATA_PIN PORTD5
+#define MOTOR_3_REVERSE_OUTPUT_COMPARE OCR0B
 
--- src/dc-motor-controller.cpp
+++ src/dc-motor-controller.cpp
@@ -1,10 +1,13 @@
-// Add description here
+// Example code for dc-motor-controller. Adapt this to exact use.
+//
+// NOTE: Pin descriptions and so on are for 2016-05-30 revision of the printed
+// circuit, with manual fixes. Thus, not compatible with the latest revision.
 //
 // Author: Otto Urpelainen
 // Email: oturpe@iki.fi
 // Date: 2016-06-18
 
-#include "Atmega88pUtils.h"
+#include "Atmega8Utils.h"
 
 #include "config.h"
 
@@ -47,91 +50,217 @@ void readSwitches(bool * switchState) {
 /// \brief
 ///    Sets the motors to the state they should have on startup.
 void initializeMotors() {
-    // All running clockwise
+    // Set running forwards
+    MOTOR_1_FORWARD_OUTPUT_COMPARE = MOTOR_1_SPEED;
+    MOTOR_1_REVERSE_OUTPUT_COMPARE = 0x00;
+
+    MOTOR_2_FORWARD_OUTPUT_COMPARE = MOTOR_1_SPEED;
+    MOTOR_2_REVERSE_OUTPUT_COMPARE = 0x00;
+
+    MOTOR_3_FORWARD_OUTPUT_COMPARE = MOTOR_1_SPEED;
+    MOTOR_3_REVERSE_OUTPUT_COMPARE = 0x00;
+
+    // Enable all outputs
     MOTOR_1_FORWARD_DATA |= BV(MOTOR_1_FORWARD_DATA_PIN);
     MOTOR_2_FORWARD_DATA |= BV(MOTOR_2_FORWARD_DATA_PIN);
     MOTOR_3_FORWARD_DATA |= BV(MOTOR_3_FORWARD_DATA_PIN);
+    MOTOR_1_REVERSE_DATA |= BV(MOTOR_1_REVERSE_DATA_PIN);
+    MOTOR_2_REVERSE_DATA |= BV(MOTOR_2_REVERSE_DATA_PIN);
+    MOTOR_3_REVERSE_DATA |= BV(MOTOR_3_REVERSE_DATA_PIN);
 }
 
 /// \brief
-///    Implement motor 1 control logic here
+///    Calculates the correct acceleration to use when current and target speed
+///    are as given.
+int16_t acceleration(uint8_t current, uint8_t target, uint8_t maxAcceleration) {
+    int16_t acceleration;
+
+    acceleration = target - current;
+
+    if (acceleration > maxAcceleration) {
+        acceleration = maxAcceleration;
+    }
+
+    return acceleration;
+}
+
+/// \brief
+///    Changes motor 1 speed according to given speed target and maximum
+///    acceleration.
+///
+/// \param forwardTarget
+///    Forward target speed
+///
+/// \param reverseTarget
+///    Reverse target speed
+///
+/// \param acceleration
+///    Desired acceleration
+void updateMotor1Speed(
+    uint8_t forwardTarget,
+    uint8_t reverseTarget,
+    uint8_t acceleration
+) {
+    if (!MOTOR_1_REVERSE_OUTPUT_COMPARE) {
+        MOTOR_1_FORWARD_OUTPUT_COMPARE += ::acceleration(
+            MOTOR_1_FORWARD_OUTPUT_COMPARE,
+            forwardTarget,
+            acceleration
+        );
+    }
+    if (!MOTOR_1_FORWARD_OUTPUT_COMPARE) {
+        MOTOR_1_REVERSE_OUTPUT_COMPARE += ::acceleration(
+            MOTOR_1_REVERSE_OUTPUT_COMPARE,
+            reverseTarget,
+            acceleration
+        );
+    }
+}
+
+/// \brief
+///    Changes motor 2 speed according to given speed target and maximum
+///    acceleration.
+///
+/// See updateMotor1Speed() function documentation for details. This function
+/// behaves identically to that one.
+void updateMotor2Speed(
+    uint8_t forwardTarget,
+    uint8_t reverseTarget,
+    uint8_t acceleration
+) {
+    if (!MOTOR_2_REVERSE_OUTPUT_COMPARE) {
+        MOTOR_2_FORWARD_OUTPUT_COMPARE += ::acceleration(
+            MOTOR_2_FORWARD_OUTPUT_COMPARE,
+            forwardTarget,
+            acceleration
+        );
+    }
+    if (!MOTOR_2_FORWARD_OUTPUT_COMPARE) {
+        MOTOR_2_REVERSE_OUTPUT_COMPARE += ::acceleration(
+            MOTOR_2_REVERSE_OUTPUT_COMPARE,
+            reverseTarget,
+            acceleration
+        );
+    }
+}
+
+/// \brief
+///    Changes motor 3 speed according to given speed target and maximum
+///    acceleration.
+///
+/// See updateMotor1Speed() function documentation for details. This function
+/// behaves identically to that one.
+void updateMotor3Speed(
+    uint8_t forwardTarget,
+    uint8_t reverseTarget,
+    uint8_t acceleration
+) {
+    if (!MOTOR_3_REVERSE_OUTPUT_COMPARE) {
+        MOTOR_3_FORWARD_OUTPUT_COMPARE += ::acceleration(
+            MOTOR_3_FORWARD_OUTPUT_COMPARE,
+            forwardTarget,
+            acceleration
+        );
+    }
+    if (!MOTOR_3_FORWARD_OUTPUT_COMPARE) {
+        MOTOR_3_REVERSE_OUTPUT_COMPARE += ::acceleration(
+            MOTOR_3_REVERSE_OUTPUT_COMPARE,
+            reverseTarget,
+            acceleration
+        );
+    }
+}
+
+/// \brief
+///    Sets motor 1 rotation according to given switch press information.
+///
+/// This function stores desired motor state as static variables. Every time
+/// this function is called, desired state is updated if switch press
+/// information requires this. Otherwise, motor speed is increased by motor
+/// acceleration value as stored in config header.
 ///
 /// \param clockwiseEnd
 ///    If the switch in end of clockwise motion is pressed
 ///
 /// \param counterClockwiseEnd
 ///    If the switch in end of counter-clockwise motion is pressed
-
-
-
 void controlMotor1(bool clockwiseEnd, bool counterClockwiseEnd) {
-        // In clockwise end: change direction
-        if(clockwiseEnd && !counterClockwiseEnd) {
-            MOTOR_1_FORWARD_DATA |= BV(MOTOR_1_FORWARD_DATA_PIN);
-            MOTOR_1_REVERSE_DATA &= ~BV(MOTOR_1_REVERSE_DATA_PIN);;
-        }
-        // In counter-clockwise end: change direction
-        else if(!clockwiseEnd && counterClockwiseEnd) {
-            MOTOR_1_FORWARD_DATA &= ~BV(MOTOR_1_FORWARD_DATA_PIN);
-            MOTOR_1_REVERSE_DATA |= BV(MOTOR_1_REVERSE_DATA_PIN);;
-        }
-        // Should not happen: stop motor
-        else if (clockwiseEnd && counterClockwiseEnd) {
-            MOTOR_1_FORWARD_DATA &= ~BV(MOTOR_1_FORWARD_DATA_PIN);
-            MOTOR_1_REVERSE_DATA &= ~BV(MOTOR_1_REVERSE_DATA_PIN);;
-        }
+    static uint8_t forwardTarget = MOTOR_1_SPEED;
+    static uint8_t reverseTarget = 0x00;
+
+    // In clockwise end: change direction
+    if(clockwiseEnd && !counterClockwiseEnd) {
+        forwardTarget = MOTOR_1_SPEED;
+        reverseTarget = 0x00;
+    }
+    // In counter-clockwise end: change direction
+    else if(!clockwiseEnd && counterClockwiseEnd) {
+        forwardTarget = 0x00;
+        reverseTarget = MOTOR_1_SPEED;
+    }
+    // Should not happen: stop motor
+    else if (clockwiseEnd && counterClockwiseEnd) {
+        forwardTarget = 0x00;
+        reverseTarget = 0x00;
+    }
+
+    updateMotor1Speed(forwardTarget, reverseTarget, MOTOR_1_ACCELERATION);
 }
 
 /// \brief
-///    Implement motor 2 control logic here
+///    Sets motor 2 rotation according to given switch press information.
 ///
-/// \param clockwiseEnd
-///    If the switch in end of clockwise motion is pressed
-///
-/// \param counterClockwiseEnd
-///    If the switch in end of counter-clockwise motion is pressed
+/// See controlMotor2() function documentation for details. This function
+/// behaves identically to that one.
 void controlMotor2(bool clockwiseEnd, bool counterClockwiseEnd) {
-        // In clockwise end: change direction
-        if(clockwiseEnd && !counterClockwiseEnd) {
-            MOTOR_2_FORWARD_DATA |= BV(MOTOR_2_FORWARD_DATA_PIN);
-            MOTOR_2_REVERSE_DATA &= ~BV(MOTOR_2_REVERSE_DATA_PIN);
-        }
-        // In counter-clockwise end: change direction
-        else if(!clockwiseEnd && counterClockwiseEnd) {
-            MOTOR_2_FORWARD_DATA &= ~BV(MOTOR_2_FORWARD_DATA_PIN);
-            MOTOR_2_REVERSE_DATA |= BV(MOTOR_2_REVERSE_DATA_PIN);
-        }
-        // Should not happen: stop motor
-        else if (clockwiseEnd && counterClockwiseEnd) {
-            MOTOR_2_FORWARD_DATA &= ~BV(MOTOR_2_FORWARD_DATA_PIN);
-            MOTOR_2_REVERSE_DATA &= ~BV(MOTOR_2_REVERSE_DATA_PIN);
-        }
+    static uint8_t forwardTarget = MOTOR_2_SPEED;
+    static uint8_t reverseTarget = 0x00;
+
+    // In clockwise end: change direction
+    if(clockwiseEnd && !counterClockwiseEnd) {
+        forwardTarget = MOTOR_2_SPEED;
+        reverseTarget = 0x00;
+    }
+    // In counter-clockwise end: change direction
+    else if(!clockwiseEnd && counterClockwiseEnd) {
+        forwardTarget = 0x00;
+        reverseTarget = MOTOR_2_SPEED;
+    }
+    // Should not happen: stop motor
+    else if (clockwiseEnd && counterClockwiseEnd) {
+        forwardTarget = 0x00;
+        reverseTarget = 0x00;
+    }
+
+    updateMotor2Speed(forwardTarget, reverseTarget, MOTOR_2_ACCELERATION);
 }
 
 /// \brief
-///    Implement motor 3 control logic here
+///    Sets motor 3 rotation according to given switch press information.
 ///
-/// \param clockwiseEnd
-///    If the switch in end of clockwise motion is pressed
-///
-/// \param counterClockwiseEnd
-///    If the switch in end of counter-clockwise motion is pressed
+/// See controlMotor3() function documentation for details. This function
+/// behaves identically to that one.
 void controlMotor3(bool clockwiseEnd, bool counterClockwiseEnd) {
-        // In clockwise end: change direction
-        if(clockwiseEnd && !counterClockwiseEnd) {
-            MOTOR_3_FORWARD_DATA |= BV(MOTOR_3_FORWARD_DATA_PIN);
-            MOTOR_3_REVERSE_DATA &= ~BV(MOTOR_3_REVERSE_DATA_PIN);
-        }
-        // In counter-clockwise end: change direction
-        else if(!clockwiseEnd && counterClockwiseEnd) {
-            MOTOR_3_FORWARD_DATA &= ~BV(MOTOR_3_FORWARD_DATA_PIN);
-            MOTOR_3_REVERSE_DATA |= BV(MOTOR_3_REVERSE_DATA_PIN);
-        }
-        // Should not happen: stop motor
-        else if (clockwiseEnd && counterClockwiseEnd) {
-            MOTOR_3_FORWARD_DATA &= ~BV(MOTOR_3_FORWARD_DATA_PIN);
-            MOTOR_3_REVERSE_DATA &= ~BV(MOTOR_3_REVERSE_DATA_PIN);
-        }
+    static uint8_t forwardTarget = MOTOR_3_SPEED;
+    static uint8_t reverseTarget = 0x00;
+
+    // In clockwise end: change direction
+    if(clockwiseEnd && !counterClockwiseEnd) {
+        forwardTarget = MOTOR_3_SPEED;
+        reverseTarget = 0x00;
+    }
+    // In counter-clockwise end: change direction
+    else if(!clockwiseEnd && counterClockwiseEnd) {
+        forwardTarget = 0x00;
+        reverseTarget = MOTOR_3_SPEED;
+    }
+    // Should not happen: stop motor
+    else if (clockwiseEnd && counterClockwiseEnd) {
+        forwardTarget = 0x00;
+        reverseTarget = 0x00;
+    }
+
+    updateMotor3Speed(forwardTarget, reverseTarget, MOTOR_3_ACCELERATION);
 }
 
 int main() {
@@ -150,6 +279,30 @@ int main() {
     MOTOR_2_REVERSE_DATA_DIR |= BV(MOTOR_2_REVERSE_DATA_DIR_PIN);
     MOTOR_3_FORWARD_DATA_DIR |= BV(MOTOR_3_FORWARD_DATA_DIR_PIN);
     MOTOR_3_REVERSE_DATA_DIR |= BV(MOTOR_3_REVERSE_DATA_DIR_PIN);
+
+    Atmega8::initializeTimer0(
+        PRESCALER_VALUE,
+        Atmega8::PWM_PHASE_CORRECT,
+        Atmega8::TOP_00FF
+    );
+    Atmega8::initializeTimer1(
+        PRESCALER_VALUE,
+        Atmega8::PWM_PHASE_CORRECT,
+        Atmega8::TOP_00FF
+    );
+    Atmega8::initializeTimer2(
+        PRESCALER_VALUE,
+        Atmega8::PWM_PHASE_CORRECT,
+        Atmega8::TOP_00FF
+    );
+
+    OCR1AH = 0x00;
+    OCR1BH = 0x00;
+
+    // Set non-inverting pwm
+    TCCR0A |= BV(COM0A1) | BV(COM0B1);
+    TCCR1A |= BV(COM1A1) | BV(COM1B1);
+    TCCR2A |= BV(COM2A1) | BV(COM2B1);
 
     initializeMotors();
 
